a5d78be19f795db8a4f7285ed0407f4811b7ac3a
TopRamen1 <nicolas.duc@op.pl>
2021-10-12 16:52:28 +0200

    Extracted tf03 base as a virtual class to be shared by CAN and Serial classes

---
+++ tf03_driver/tf03_driver/include/TF03_Base.h
@@ -0,0 +1,70 @@
+#include <math.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "ros/ros.h"
+#include "sensor_msgs/Range.h"
+
+#ifndef TF03_BASE_H
+#define TF03_BASE_H
+
+#define SET_OUTPUT_FORMAT_SERIAL 0x01
+#define SET_OUTPUT_FORMAT_CAN 0x02
+
+enum tf_03_interface
+{
+    serial = 0,
+    can = 1
+};
+
+enum tf_03_command_id
+{
+    version = 0x01,
+    system_reset = 0x02,
+    framerate = 0x03,
+    factory_settings = 0x10,
+    save_settings = 0x11,
+    output_format = 0x45,
+    transmit_can_id = 0x50,
+    receive_can_id = 0x51,
+};
+
+struct parameter_config
+{
+    bool frame_sent;
+    bool command_success;
+    tf_03_command_id command;
+    int64_t argument;
+};
+
+class TF03_Base
+{
+public:
+    TF03_Base(ros::NodeHandle nh) {node_handle = nh;};
+    virtual ~TF03_Base() {};
+
+    u_char get_checksum(std::vector<u_char> data);
+    void process_incoming_buffer(std::vector<u_char> data, int can_id = 0);
+
+
+    // ROS
+    ros::NodeHandle node_handle;
+    std::map<int, ros::Publisher> sensor_pub;
+    std::map<int, sensor_msgs::Range> sensor_data;
+
+    std::vector<parameter_config> parameters;
+    std::vector<u_char> incoming_buffer;
+
+private:
+    virtual int init_sensor(std::string device_name) = 0;
+    virtual void send_command(tf_03_command_id command_id, int64_t command_argument = 0) = 0;
+    virtual void write_command_data(std::vector<u_char> data) = 0;
+    virtual void process_sensor_data() = 0;
+
+    void clear_incoming_buffer();
+    bool is_buffer_correct(std::vector<u_char> *data);
+    bool verify_checksum(std::vector<u_char> data);
+};
+
+#endif
--- tf03_driver/tf03_driver/include/TF03_CAN.h
+++ tf03_driver/tf03_driver/include/TF03_CAN.h
@@ -1,7 +1,3 @@
-#include <math.h>
-#include <iostream>
-#include <string>
-#include <vector>
 #include <sys/socket.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
@@ -11,52 +7,25 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Range.h"
 
+#include "TF03_Base.h"
+
 #ifndef TF03_CAN_H
 #define TF03_CAN_H
 
 #define SET_OUTPUT_FORMAT_CAN 0x02
 
-enum tf_03_command_id
-{
-    version = 0x01,
-    system_reset = 0x02,
-    framerate = 0x03,
-    factory_settings = 0x10,
-    save_settings = 0x11,
-    output_format = 0x45,
-    transmit_can_id = 0x50,
-    receive_can_id = 0x51,
-};
 
-struct parameter_config
-{
-    bool frame_sent;
-    bool command_success;
-    tf_03_command_id command;
-    int64_t argument;
-};
-
-class TF03_CAN
+class TF03_CAN: private TF03_Base
 {
 public:
     TF03_CAN(ros::NodeHandle nh);
-    ~TF03_CAN();
+    virtual ~TF03_CAN();
 
 private:
-    int init_sensor(std::string device_name);
-    void send_command(tf_03_command_id command_id, int64_t command_argument = 0);
-    void write_command_data(std::vector<u_char> data);
-    void clear_incoming_buffer();
-    void process_incoming_buffer(std::vector<u_char> data, int can_id = 0);
-    bool is_buffer_correct(std::vector<u_char> *data);
-    bool verify_checksum(std::vector<u_char> data);
-    u_char get_checksum(std::vector<u_char> data);
-    void process_sensor_data();
-
-    // ROS
-    ros::NodeHandle node_handle;
-    std::map<int, ros::Publisher> sensor_pub;
-    std::map<int, sensor_msgs::Range> sensor_data;
+    int init_sensor(std::string device_name) override;
+    void send_command(tf_03_command_id command_id, int64_t command_argument = 0) override;
+    void write_command_data(std::vector<u_char> data) override;
+    void process_sensor_data() override;
 
     // Params
     std::string can_device;
@@ -65,22 +34,10 @@ private:
     std::vector<std::string> sensor_frame;
     std::map<int, std::string> sensors;
 
-    
-    std::string line;
-    u_char read_byte;
-    uint32_t timeout_ms;
-    std::vector<u_char> incoming_buffer;
-    u_char low_byte;
-    u_char high_byte;
-    u_char checksum;
-    u_char checksum_byte;
-    uint16_t dist;
-    float dist_meters;
     struct can_frame *frame;
     int64_t can_socket;
 
     ros::Time command_timestamp;
     static std::map<tf_03_command_id, u_char> tf_03_command_len;
-    std::vector<parameter_config> parameters;
 };
 #endif
